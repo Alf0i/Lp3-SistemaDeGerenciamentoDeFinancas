aac5348a7ba88ba5566084396f1209312ee46fc4
Joseviniciusrs <jvschweitzer@gmail.com>
2023-10-29 16:34:26 -0300

    Interfaces
    Criação das interfaces de Usuário, despesas e Receita.

---
+++ SistemaDeGerenc/SistemaDeGerenc.BLL/IDespesa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeGerenc.BLL
+{
+    internal interface IDespesa
+    {
+        void CalcularDespesa();
+    }
+}
---
+++ SistemaDeGerenc/SistemaDeGerenc.BLL/IUsuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeGerenc.BLL
+{
+    internal interface IUsuario
+    {
+
+        string? Login { get; set; }
+        string? Nome { get; set; }
+        string? Senha { get; set; }
+
+        void CadastrarUsuario();
+        void RemoverUsuario();
+        void TrocarSenha();
+    }
+}
